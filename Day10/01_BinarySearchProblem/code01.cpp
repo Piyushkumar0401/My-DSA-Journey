00ab5b75f45c96e0dd2a42022846fb625a23701b
Piyush Kumar <jhapiyush803@gmail.com>
2025-07-30 20:36:36 +0530

    Day 10

---
+++ Day10/01_BinarySearchProblem/code01.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+void firstAndLast(int arr[], int key, int n)
+{
+  int start = 0;
+  int end = n - 1;
+  int first, last;
+  int mid = start + (end - start) / 2;
+  while (start <= end)
+  {
+
+    if (arr[mid] == key)
+    {
+      first = mid;
+      end = mid - 1;
+    }
+    else if (arr[mid] < key)
+    {
+      start = mid + 1;
+    }
+    else
+    {
+      end = mid - 1;
+    }
+    mid = start + (end - start) / 2;
+  }
+  cout << first<<" ";
+  start = 0;
+  end = n - 1;
+  mid = start + (end - start) / 2;
+  while (start <= end)
+  {
+    if (arr[mid] == key)
+    {
+      last = mid;
+      start = mid + 1;
+    }
+    else if (arr[mid] < key)
+    {
+      start = mid + 1;
+    }
+    else
+    {
+      end = mid - 1;
+    }
+    mid = start + (end - start) / 2;
+  }
+  cout << last<<endl;
+  cout<<" the total number of occurence : "<<(last-first)<<endl;
+}
+
+int main()
+{
+  int even[] = {1, 2, 2, 2, 2, 2, 2, 8}; // output must be 1 - 6
+  int odd[] = {1, 2, 2, 2, 2, 2, 2};     // output must be 1 - 6
+  int key;
+  cout << "Enter the number : ";
+  cin >> key;
+  int n1 = sizeof(even) / sizeof(even[0]);
+  int n2 = sizeof(odd) / sizeof(odd[0]);
+  firstAndLast(even, key, n1);
+  firstAndLast(odd, key, n2);
+  return 0;
+}
