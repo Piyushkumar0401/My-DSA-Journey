64c77b9b8e09887ccb51f8c894c785d99a4e413d
Piyush Kumar <jhapiyush803@gmail.com>
2025-08-11 18:44:15 +0530

    Day 22\

---
+++ Day22/code01.cpp
@@ -0,0 +1,34 @@
+// GCD -> EUCLID'S ALGORITHM
+
+#include<iostream>
+using namespace std;
+int gcd(int a , int b){
+  if(a==0) return b;
+  if(b==0) return a;
+
+  while (a!=b)
+  {
+    if(a>b){
+      a = a - b;
+    }
+    else{
+      b = b -a;
+    }
+  }
+  return a;
+  
+}
+
+
+
+int main(){
+int a,b;
+cout<<"enter a : ";
+cin>>a;
+cout<<"enter b : ";
+cin>>b;
+
+int ans = gcd(a,b);
+cout<<"gcd of a and b is : "<<ans;
+  return 0;
+}
---
+++ Day22/code02.cpp
@@ -0,0 +1,37 @@
+// LCM -> EUCLID'S ALGORITHM
+
+#include<iostream>
+using namespace std;
+int gcd(int a , int b){
+  if(a==0) return b;
+  if(b==0) return a;
+
+  while (a!=b)
+  {
+    if(a>b){
+      a = a - b;
+    }
+    else{
+      b = b -a;
+    }
+  }
+  return a;
+  
+}
+int lcm(int a,int b){
+return (a * b) / gcd(a,b);
+}
+
+
+
+int main(){
+int a,b;
+cout<<"enter a : ";
+cin>>a;
+cout<<"enter b : ";
+cin>>b;
+
+int ans = lcm(a,b);
+cout<<"lcm of a and b is : "<<ans;
+  return 0;
+}
