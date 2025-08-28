0cd5c6d769d2ec709a2e219db13321bd5dddcf58
Piyush Kumar <jhapiyush803@gmail.com>
2025-08-28 20:40:01 +0530

    Day 39

---
+++ Day39/02_CircularLinkedList/code01.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+  int data;
+  Node *next;
+
+  Node(int data)
+  {
+    this->data = data;
+    this->next = NULL;
+  }
+  ~Node()
+  {
+    int value = this->data;
+    if (this->next == NULL)
+    {
+      delete next;
+      next = NULL;
+      cout << "Memory is Free  for node with Data " << value << endl;
+    }
+  }
+};
+// void insertAtHead() {}
+// void insertAtTail() {}
+// void insertAtPositon() {}
+// void deleteNode() {}
+// void getLength() {}
+// void print() {}
+
+void insertNode(Node *&tail, int element, int d)
+{
+  // Assuming that the element is present in the list
+
+  // empty list
+  if (tail == NULL)
+  {
+    Node *node1 = new Node(d);
+    tail = node1;
+    node1->next = node1;
+  }
+  else
+  { // non empty list
+    Node *curr = tail;
+    while (curr->data != element)
+    {
+      curr = curr->next;
+    }
+
+    // element found -> curr is represent that element node
+    Node *temp = new Node(d);
+    temp->next = curr->next;
+    curr->next = temp;
+  }
+}
+void deletionNode(Node *&tail, int element)
+{
+  if (tail == NULL)
+  {
+    cout << "List is empty " << endl;
+    return;
+  }
+  else
+  {
+    Node *prev = tail;
+    Node *curr = prev->next;
+    while (curr->data != element)
+    {
+      prev = curr;
+      curr = curr->next;
+    }
+    prev->next = curr->next;
+    if (curr == prev)
+    {
+      tail = NULL;
+    }
+    else if (tail == curr)
+    {
+      tail = prev;
+    }
+    curr->next = NULL;
+    delete curr;
+  }
+}
+void print(Node *&tail)
+{
+  Node *temp = tail;
+  if (temp == NULL)
+  {
+    cout << "List is Empty " << endl;
+  }
+  do
+  {
+    cout << tail->data << " ";
+    tail = tail->next;
+  } while (tail != temp);
+  cout << endl;
+}
+
+int main()
+{
+  // Node *node1 = new Node(10);
+
+  // Node *head = node1;  iski jarurat nahi padti yaha
+  Node *tail = NULL;
+  insertNode(tail, 5, 10);
+  print(tail);
+  insertNode(tail, 10, 20);
+  print(tail);
+  insertNode(tail, 20, 40);
+  print(tail);
+  insertNode(tail, 20, 30);
+  print(tail);
+  // deletionNode(tail, 40);
+  deletionNode(tail, 10);
+  print(tail);
+
+  return 0;
+}
---
+++ Day39/04_MiddleOfLinkedList/code01.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+using namespace std;
+
+class Link
+{
+public:
+  int data;
+  Link *next;
+
+  Link(int data)
+  {
+    this->data = data;
+    this->next = NULL;
+  }
+};
+
+// Middle of Linked List -> data bta rha h
+int middleOfLinkedList(Link *&head, int len)
+{
+  Link *temp1 = head;
+  int position = len / 2 + 1;
+  int cnt = 1;
+  while (cnt < position)
+  {
+    head = head->next;
+    cnt++;
+  }
+  return head->data;
+}
+int getLength(Link *&head)
+{
+  Link *temp = head;
+  int cnt = 0;
+  while (temp != NULL)
+  {
+    temp = temp->next;
+    cnt++;
+  }
+  return cnt;
+}
+void print(Link *&head)
+{
+  Link *l2 = head;
+  while (l2 != NULL)
+  {
+    cout << l2->data << " ";
+    l2 = l2->next;
+  }
+  cout << endl;
+}
+
+void insertAtTail(Link *&tail, int data)
+{
+  Link *l2 = new Link(data);
+  tail->next = l2;
+  tail = tail->next;
+}
+void InsertAtHead(Link *&head, int data)
+{
+
+  // create a new node
+  Link *node2 = new Link(data);
+
+  node2->next = head; // n2->Node mai maine second wale ka head de diya
+  head = node2;
+}
+void insertAtPosition(Link *&head, Link *&tail, int Position, int d)
+{
+  // insert at start
+  if (Position == 1)
+  {
+    InsertAtHead(head, d);
+    return;
+  }
+  Link *temp = head;
+  int cnt = 1;
+  while (cnt < Position - 1)
+  {
+    temp = temp->next;
+    cnt++;
+  }
+  // insert at last
+  if (temp->next == NULL)
+  {
+    insertAtTail(tail, d);
+    return;
+  }
+  // creating a node for d
+  Link *nodeToInsert = new Link(d);
+  nodeToInsert->next = temp->next;
+  temp->next = nodeToInsert;
+}
+
+int main()
+{
+  Link *l1 = new Link(10);
+  Link *head = l1;
+  Link *tail = l1;
+  insertAtTail(tail, 20);
+  insertAtTail(tail, 40);
+  insertAtTail(tail, 50);
+  insertAtPosition(head, tail, 3, 30);
+  insertAtTail(tail, 60);
+  insertAtTail(tail, 70);
+  insertAtTail(tail, 80);
+  print(head);
+  int len = getLength(head);
+  cout << "Middle of the LinkedList : " << middleOfLinkedList(head, len) << endl;
+  return 0;
+}
