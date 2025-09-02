11c4679bc0bee98b83af750c63d56aaa746b1126
Piyush Kumar <jhapiyush803@gmail.com>
2025-09-02 16:07:01 +0530

    Day 45

---
+++ Day44/01_2StackInAnArray/code01.cpp
@@ -0,0 +1,1 @@
+// https://bit.ly/3gr4wW8
---
+++ Day44/02_ReverseStringUsingStack/code01.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+
+
+int main(){
+
+  string str = "babbar";
+  stack<int>s;
+  int top = -1;
+  int len = str.length();
+  for (int i = 0; i < len; i++)
+  {
+    top++;
+    s.push(str[i]);
+  }
+  string ans = "";
+  while(!s.empty())
+  {
+    char remo = s.top();
+    ans = ans + remo;
+    s.pop();
+  }
+  cout<<ans;
+    
+  return 0;
+}
